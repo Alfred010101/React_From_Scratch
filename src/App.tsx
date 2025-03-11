9478efd1ca8580ff489dbe7addb229225c8a8dd5
Alfred <arista21dominguez@gmail.com>
2025-03-10 19:04:28 -0600

    Componentes II

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,28 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
 function App() {
-  const list = [
-    "Persona 1",
-    "Persona 2",
-    "Persona 3",
-    "Persona 4",
-    "Persona 5",
+  const list: string[] = [
+    // "Persona 1",
+    // "Persona 2",
+    // "Persona 3",
+    // "Persona 4",
+    // "Persona 5",
   ];
+
+  const handleSelect = (elemento: string) => {
+    console.log(elemento);
+  };
+
+  const contenido = list.length ? (
+    <List data={list} onSelect={handleSelect} />
+  ) : (
+    "Sin elemenos"
+  );
+
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto" />
-      <List data={list} />
+      {contenido}
     </Card>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,18 +2,20 @@ import { useState } from "react";
 
 type Props = {
   data: string[];
+  onSelect?: (element: string) => void;
 };
 
-function List({ data }: Props) {
+function List({ data, onSelect }: Props) {
   const [index, setIndex] = useState(0);
-  const handleClik = (i: number) => {
+  const handleClik = (i: number, e: string) => {
     setIndex(i);
+    onSelect?.(e);
   };
   return (
     <ul className="list-group">
       {data.map((element, i) => (
         <li
-          onClick={() => handleClik(i)}
+          onClick={() => handleClik(i, element)}
           key={element}
           className={`list-group-item ${index == i ? "active" : ""}`}
         >
